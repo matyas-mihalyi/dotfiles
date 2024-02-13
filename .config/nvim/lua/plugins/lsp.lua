fdcac6e35439f57e1c3fa0a5af66d608df5a9cd8
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2024-02-13 21:22:54 +0100

    ensure installed jsonls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
     'williamboman/mason-lspconfig.nvim',
     config = function()
       require('mason-lspconfig').setup({
-        ensure_installed = { 'lua_ls', 'tsserver', 'clangd', 'html', 'cssls', 'eslint' }
+        ensure_installed = { 'lua_ls', 'tsserver', 'clangd', 'html', 'cssls', 'eslint', 'jsonls' }
       })
     end
   },
