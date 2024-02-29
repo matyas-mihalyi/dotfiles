b3248f517de3d1eb1cd9b789ea0ac8749ab72ae3
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2024-02-29 09:10:49 +0100

    you think we're done?

--- .config/nvim/lua/matyi/remap.lua
+++ .config/nvim/lua/matyi/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
+
 -- panes 
 vim.keymap.set("n", "<leader>l", "<C-w>l")
 vim.keymap.set("n", "<leader>L", "<C-w>L")
@@ -9,7 +10,16 @@ vim.keymap.set("n", "<leader>j", "<C-w>j")
 vim.keymap.set("n", "<leader>J", "<C-w>J")
 vim.keymap.set("n", "<leader>k", "<C-w>k")
 vim.keymap.set("n", "<leader>K", "<C-w>K")
+
+-- goto definition in new right side pane
+vim.keymap.set('n', 'gds', ':bo vs | lua vim.lsp.buf.definition()<CR>')
+
 -- exit inser mode
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- center view
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
 
