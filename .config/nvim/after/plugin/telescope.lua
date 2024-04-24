9fc8276f3783e3a16a53396281bef66e6b705d63
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2024-04-24 20:53:31 +0200

    yaya

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>FF', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>st', builtin.git_status, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>ff', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
 end)
